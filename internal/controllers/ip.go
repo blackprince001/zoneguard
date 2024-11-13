5a45e98b295cf227d06299198e96410fe64216fb
blackprince001 <appiahboaduprince@gmail.com>
2024-11-13 19:38:13 +0000

    use templates to render IP address to routes

--- internal/controllers/ip.go
+++ internal/controllers/ip.go
@@ -13,5 +13,19 @@ func GetIP(c *gin.Context) {
 		c.AbortWithStatus(http.StatusInternalServerError)
 		return
 	}
-	c.JSON(http.StatusOK, ip)
+	c.JSON(http.StatusOK, gin.H{
+		"message": "success",
+		"data":    ip,
+	})
+}
+
+func Address(c *gin.Context) {
+	ip, err := grubber.GetRemoteIP(c)
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+	c.HTML(200, "ip.tmpl", gin.H{
+		"ip_address": ip.IpAddr,
+	})
 }
