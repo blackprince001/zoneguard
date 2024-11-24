7b9a4c70ff4d91bd4640dac6aef7be61356a61d6
blackprince001 <appiahboaduprince@gmail.com>
2024-11-24 23:02:34 +0000

    open sqlite, migrate iplocator model, move csv items to database, start server

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
+	"path/filepath"
+
 	"github.com/gin-gonic/gin"
 
+	"log"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+
 	controllers "zoneguard/internal/controllers"
+	"zoneguard/internal/database"
 )
 
 func main() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	}
+
+	db.AutoMigrate(&database.Iplocator{})
+
+	csv_file := filepath.Join(".", "data.csv")
+
+	err = database.CSVtoSqlite(db, csv_file)
+	if err != nil {
+		log.Fatal("Could not load all content of CSV to database")
+	}
+
 	r := gin.Default()
 
 	r.LoadHTMLGlob("templates/*")
