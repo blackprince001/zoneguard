8b2e345fa0625899dcd7440de509c3f724e7e30f
blackprince001 <appiahboaduprince@gmail.com>
2024-11-27 23:55:00 +0000

    create endpoint controller for getting geo information about ip address

--- internal/controllers/ip.go
+++ internal/controllers/ip.go
@@ -2,10 +2,16 @@ package controllers
 
 import (
 	"net/http"
+	"strconv"
+	"strings"
+	"zoneguard/internal/database"
 	"zoneguard/internal/grubber"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
+
+var db *gorm.DB
 
 func GetIP(c *gin.Context) {
 	ip, err := grubber.GetRemoteIP(c)
@@ -16,6 +22,35 @@ func GetIP(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "success",
 		"data":    ip,
+	})
+}
+
+func GetIpGeoLocation(c *gin.Context) {
+	ip, err := grubber.GetRemoteIP(c)
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	var geoData database.Iplocator
+
+	related_id := strings.Join(strings.Split(ip.IpAddr, "."), "")
+	relatedID, err := strconv.ParseUint(related_id, 10, 64)
+	if err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	err = db.Where("related_id = ?", relatedID).Find(&geoData).Error
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "success",
+		"data":    ip,
+		"geo":     geoData,
 	})
 }
 
