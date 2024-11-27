80a88207ee7c1ed8afc8b9504d2f106c694bd376
blackprince001 <appiahboaduprince@gmail.com>
2024-11-27 23:54:37 +0000

    complete get method for geo-ip

--- internal/database/db.go
+++ internal/database/db.go
@@ -6,22 +6,26 @@ import (
 
 type IplocatorInterface interface {
 	CreateIplocatorItem(geo_ip *Iplocator) error
-	GetIplocatorItem(id uint) error
+	GetIplocatorItem(id uint64) (*Iplocator, error)
 }
 
 type IplocatorRepo struct {
 	db *gorm.DB
 }
 
-func NewIplocatorRepo(db *gorm.DB) IplocatorInterface {
-	return &IplocatorRepo{db: db}
+func NewIplocatorRepo(db *gorm.DB) IplocatorRepo {
+	return IplocatorRepo{db: db}
 }
 
 func (r *IplocatorRepo) CreateIplocatorItem(geo_ip *Iplocator) error {
 	return r.db.Create(geo_ip).Error
 }
 
-// GetIplocatorItem implements IplocatorInterface.
-func (r *IplocatorRepo) GetIplocatorItem(id uint) error {
-	panic("unimplemented")
+func (r *IplocatorRepo) GetIplocatorItem(id uint64) (*Iplocator, error) {
+	var geo_ip Iplocator
+	err := r.db.Where("related_id = ?", id).Find(&geo_ip).Error
+	if err != nil {
+		return nil, err
+	}
+	return &geo_ip, nil
 }
